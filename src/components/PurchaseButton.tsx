67e31fc9f4aa78696e15229724ffd89167fe11f9
t-tajiri <swallow.ava@gmail.com>
2022-04-24 16:48:27 +0900

    feat: 注文回数の表示およびカウンターの実装

--- src/components/PurchaseButton.tsx
+++ src/components/PurchaseButton.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type Props = {
   id: string;
   text: string;
@@ -6,20 +8,32 @@ type Props = {
 };
 
 export const PurchaseButton: React.FC<Props> = (ctx) => {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <button
-      id={ctx.id}
-      className="bg-white hover:bg-gray-100
+    <div className="relative">
+      <button
+        id={ctx.id}
+        className="bg-white hover:bg-gray-100
                  border border-black
                  py-2 px-4
                  rounded
                  shadow
                 "
-    >
-      <div className="w-32 flex justify-between">
-        <span>{ctx.text}</span>
-        <span>{`${ctx.price} ${ctx.currencyType}`}</span>
+        onClick={() => setCounter(counter + 1)}
+      >
+        <div className="w-32 flex justify-between">
+          <span>{ctx.text}</span>
+          <span>{`${ctx.price} ${ctx.currencyType}`}</span>
+        </div>
+      </button>
+      <div
+        id={`${ctx.id}-count`}
+        data-testid={`${ctx.id}-count`}
+        className="absolute h-7 w-7 -top-3 -right-3 text-center border bg-red-500 text-white rounded-full"
+      >
+        {counter}
       </div>
-    </button>
+    </div>
   );
 };
