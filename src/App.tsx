5db43edeb4e9a7763e133864b4acb982c2b5b74e
t-tajiri <swallow.ava@gmail.com>
2022-04-24 15:43:19 +0900

    refactor: 購入ボタンのコンポーネント化

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,44 @@
-export function App() {
+import { PurchaseButton } from "./components/PurchaseButton";
+
+export const CURRENCY_TYPE = "円";
+
+export const App: React.FC = () => {
   return (
     <div className="flex h-screen">
       <div className="m-auto">
         <div className="flex flex-col gap-5">
-          <button
-            id="coffee"
-            className="bg-white hover:bg-gray-100 border border-black py-2 px-4 rounded shadow"
-          >
-            <div className="w-32 flex justify-between">
-              <span>コーヒー</span>
-              <span>480 円</span>
-            </div>
-          </button>
-          <button
-            id="tea"
-            className="bg-white hover:bg-gray-100 border border-black py-2 px-4 rounded shadow"
-          >
-            <div className="w-32 flex justify-between">
-              <span>紅茶</span>
-              <span>280 円</span>
-            </div>
-          </button>
-          <button
-            id="milk"
-            className="bg-white hover:bg-gray-100 border border-black py-2 px-4 rounded shadow"
-          >
-            <div className="w-32 flex justify-between">
-              <span>ミルク</span>
-              <span>180 円</span>
-            </div>
-          </button>
-          <button
-            id="coke"
-            className="bg-white hover:bg-gray-100 border border-black py-2 px-4 rounded shadow"
-          >
-            <div className="w-32 flex justify-between">
-              <span>コーラ</span>
-              <span>190 円</span>
-            </div>
-          </button>
-          <button
-            id="beer"
-            className="bg-white hover:bg-gray-100 border border-black py-2 px-4 rounded shadow"
-          >
-            <div className="w-32 flex justify-between">
-              <span>ビール</span>
-              <span>580 円</span>
-            </div>
-          </button>
+          <PurchaseButton
+            id={"coffee"}
+            text={"コーヒー"}
+            price={480}
+            currencyType={CURRENCY_TYPE}
+          />
+          <PurchaseButton
+            id={"tea"}
+            text={"紅茶"}
+            price={280}
+            currencyType={CURRENCY_TYPE}
+          />
+          <PurchaseButton
+            id={"milk"}
+            text={"ミルク"}
+            price={180}
+            currencyType={CURRENCY_TYPE}
+          />
+          <PurchaseButton
+            id={"coke"}
+            text={"コーラ"}
+            price={190}
+            currencyType={CURRENCY_TYPE}
+          />
+          <PurchaseButton
+            id={"beer"}
+            text={"ビール"}
+            price={580}
+            currencyType={CURRENCY_TYPE}
+          />
         </div>
       </div>
     </div>
   );
-}
+};
