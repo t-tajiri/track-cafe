39005fdb19857a47332d8a2d00ae5c524c04ce4c
t-tajiri <swallow.ava@gmail.com>
2022-04-24 22:14:16 +0900

    test: Recoil の Selector のキャッシュがテスト実行時に削除できなかったため、ワークアラウンドとしてファイルを分離する

--- src/App.case1.test.tsx
+++ src/App.case1.test.tsx
@@ -147,38 +147,3 @@ describe("5種類の商品購入ボタン", () => {
     expect(screen.getByTestId("beer-count")).toHaveTextContent("0");
   });
 });
-
-describe("お会計テーブル", () => {
-  it("初期値を0として商品数と合計金額が表示される", () => {
-    render(<App />);
-
-    expect(screen.getByText("商品数: 0 個")).toBeInTheDocument();
-    expect(screen.getByText("合計金額: 0 円")).toBeInTheDocument();
-  });
-
-  it("各種商品購入ボタンを押した合計が商品数に表示される", () => {
-    render(<App />);
-
-    expect(screen.getByText("商品数: 0 個")).toBeInTheDocument();
-
-    fireEvent.click(getButtonElementByText("コーヒー 480 円"));
-    fireEvent.click(getButtonElementByText("コーヒー 480 円"));
-    fireEvent.click(getButtonElementByText("コーラ 190 円"));
-
-    expect(screen.getByText("商品数: 3 個")).toBeInTheDocument();
-  });
-
-  it("各種商品購入ボタンを押した合計が合計金額に表示される", () => {
-    render(<App />);
-
-    expect(screen.getByText("商品数: 0 個")).toBeInTheDocument();
-
-    fireEvent.click(getButtonElementByText("コーヒー 480 円"));
-    fireEvent.click(getButtonElementByText("コーヒー 480 円"));
-    fireEvent.click(getButtonElementByText("ミルク 180 円"));
-    fireEvent.click(getButtonElementByText("ビール 580 円"));
-    fireEvent.click(getButtonElementByText("ビール 580 円"));
-
-    expect(screen.getByText("合計金額: 2300 円")).toBeInTheDocument();
-  });
-});
