f0d3d7a9a2cbf46d8df6b00b2c20cc40a0add333
t-tajiri <swallow.ava@gmail.com>
2022-04-24 18:25:09 +0900

    test: お会計テーブルの計算結果確認

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -148,11 +148,37 @@ describe("5種類の商品購入ボタン", () => {
   });
 });
 
-describe("商品数と合計金額", () => {
+describe("お会計テーブル", () => {
   it("初期値を0として商品数と合計金額が表示される", () => {
     render(<App />);
 
     expect(screen.getByText("商品数: 0 個")).toBeInTheDocument();
     expect(screen.getByText("合計金額: 0 円")).toBeInTheDocument();
+  });
+
+  it("各種商品購入ボタンを押した合計が商品数に表示される", () => {
+    render(<App />);
+
+    expect(screen.getByText("商品数: 0 個")).toBeInTheDocument();
+
+    fireEvent.click(getButtonElementByText("コーヒー 480 円"));
+    fireEvent.click(getButtonElementByText("コーヒー 480 円"));
+    fireEvent.click(getButtonElementByText("コーラ 190 円"));
+
+    expect(screen.getByText("商品数: 3 個")).toBeInTheDocument();
+  });
+
+  it("各種商品購入ボタンを押した合計が合計金額に表示される", () => {
+    render(<App />);
+
+    expect(screen.getByText("商品数: 0 個")).toBeInTheDocument();
+
+    fireEvent.click(getButtonElementByText("コーヒー 480 円"));
+    fireEvent.click(getButtonElementByText("コーヒー 480 円"));
+    fireEvent.click(getButtonElementByText("ミルク 180 円"));
+    fireEvent.click(getButtonElementByText("ビール 580 円"));
+    fireEvent.click(getButtonElementByText("ビール 580 円"));
+
+    expect(screen.getByText("合計金額: 1420 円")).toBeInTheDocument();
   });
 });
