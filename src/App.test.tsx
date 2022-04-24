c766218def6f5974faa587931fbb6dac4f51b1c0
t-tajiri <swallow.ava@gmail.com>
2022-04-24 14:38:44 +0900

    test: 5種類の商品購入ボタンの表示確認

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,37 @@
 import { render, screen } from "@testing-library/react";
 import { App } from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const getButtonElementByText = (text: string) =>
+  screen.getByRole("button", { name: text });
+
+describe("5種類の商品購入ボタンが表示される", () => {
+  it("コーヒー 480 円 のボタンを表示", () => {
+    render(<App />);
+
+    expect(getButtonElementByText("コーヒー 480 円")).toBeInTheDocument();
+  });
+
+  it("紅茶 280 円 のボタンを表示", () => {
+    render(<App />);
+
+    expect(getButtonElementByText("紅茶 280 円")).toBeInTheDocument();
+  });
+
+  it("ミルク 180 円 のボタンを表示", () => {
+    render(<App />);
+
+    expect(getButtonElementByText("ミルク 180 円")).toBeInTheDocument();
+  });
+
+  it("コーラ 190 円ボタンを表示", () => {
+    render(<App />);
+
+    expect(getButtonElementByText("コーラ 190 円")).toBeInTheDocument();
+  });
+
+  it("ビール 580 円ボタンを表示", () => {
+    render(<App />);
+
+    expect(getButtonElementByText("ビール 580 円")).toBeInTheDocument();
+  });
 });
